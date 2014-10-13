262c7d8c0ae7032fae348c163dfcf95c0ee1df3e
Caleb Smith <caleb@calebsmith.net>
2014-10-13 19:13:11 -0400

    Initial commit thing

---
+++ index.js
@@ -0,0 +1,26 @@
+var bot = require('fancy-groupme-bot');
+
+// local configuration read from env.
+const TOKEN = "pHRZcCOaax7eJNCNYWrT4OIvtFkzGl41pVOq3cHv"; // your groupme api token
+const GROUP = "7264366"; // the room you want to join
+const NAME = "node"; // the name of your bot
+const URL = "http://frozen-waters-9985.herokuapp.com/"; // the domain you're serving from, should be accessible by Groupme.
+const CONFIG = {token:TOKEN, group:GROUP, name:NAME, url:URL};
+
+var mybot = bot(CONFIG);
+
+mybot.on('botRegistered', function(b) {
+  console.log("I am registered");
+  b.message("WHAT UP BRO?");
+});
+
+mybot.on('botMessage', function(b, message) {
+  console.log("I got a message, fyi: " + message.name + " said " + message.text);
+  if (message.name != b.name && (message.text.search(/^!echo/) != -1)) {
+    var minusecho = message.text[4:]
+    b.message(message.name + " said " + message.text);
+  }
+});
+
+console.log("i am serving");
+mybot.serve(80);
